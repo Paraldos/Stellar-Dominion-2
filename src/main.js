a5f0ff4f22774a1dff0c873d45c7234ee0d536c8
Schiggy <hillbrecht.s@gmail.com>
2025-10-23 22:31:00 +0200

    test setup

--- src/main.js
+++ src/main.js
@@ -1,5 +1,27 @@
 import "./style.css";
 
-document.querySelector("#app").innerHTML = `
-  <h1>Bert</h1>
-`;
+console.log("test");
+
+let counter = 0;
+const counterEl = document.getElementById("counter");
+const incBtn = document.getElementById("inc");
+const loadBtn = document.getElementById("load");
+const saveBtn = document.getElementById("save");
+
+const updateDisplay = () => (counterEl.textContent = counter);
+
+incBtn.onclick = () => {
+  counter += 1;
+  updateDisplay();
+};
+
+loadBtn.onclick = () => {
+  counter = Number(localStorage.counter) || 0;
+  updateDisplay();
+};
+
+saveBtn.onclick = () => {
+  localStorage.counter = counter;
+};
+
+updateDisplay();
