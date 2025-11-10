5bfe7e98195e9d6de1d8334e7f5dcb3d8578a998
Schiggy <hillbrecht.s@gmail.com>
2025-11-10 20:45:35 +0100

    refined star modal

--- src/map/starModal/starModal.ts
+++ src/map/starModal/starModal.ts
@@ -1,7 +1,15 @@
 import Modal from "../../modal/modal";
+import "./starModal.css";
 
 export default class StarModal extends Modal {
+  solarSystem;
+
   constructor() {
     super("Star");
+
+    this.solarSystem = document.createElement("img");
+    this.solarSystem.classList.add("starModal__solarSystem");
+    this.solarSystem.src = "public/solarSystem.png";
+    this.modalContent.appendChild(this.solarSystem);
   }
 }
