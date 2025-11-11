ad5a3f68be89b774ef656981d4870e29034e9e1b
Schiggy <hillbrecht.s@gmail.com>
2025-11-11 20:14:57 +0100

    added buildings

--- src/starModal/starModal.ts
+++ src/starModal/starModal.ts
@@ -4,31 +4,40 @@ import StarData from "../data/starData";
 
 export default class StarModal extends Modal {
   starData: StarData;
-  solarSystemImage;
-  starInformation;
 
   constructor(starData: StarData) {
     super("Star");
     this.starData = starData;
-    this.solarSystemImage = this.initSolarSystemImage();
-    this.starInformation = this.initStarInformation();
+    this.addSoladSystemImage();
+    this.addStarInformation();
+    this.addBuildingSlots();
   }
 
-  initSolarSystemImage(): HTMLElement {
+  addSoladSystemImage(): void {
     const img = document.createElement("img");
     img.classList = "starModal__solarSystem";
     img.src = "public/solarSystem.png";
     this.modalContent.appendChild(img);
-    return img;
   }
 
-  initStarInformation(): HTMLElement {
+  addStarInformation(): void {
     const div = document.createElement("div");
     div.classList = "starModal__starInformation";
-    div.innerHTML = `
-		<p>Star Title</p>
-	`;
+    div.innerHTML = `<p>Star Title</p>`;
     this.modalContent.appendChild(div);
-    return div;
+  }
+
+  addBuildingSlots() {
+    const btns = document.createElement("div");
+    btns.innerHTML = `<h2>Buildings</h2>`;
+    btns.classList = "starModal__buildings";
+    this.modalContent.appendChild(btns);
+
+    for (let i = 0; i < 9; i++) {
+      const btn = document.createElement("button");
+      btn.classList = "starModal__building";
+      btn.innerHTML = "Building";
+      btns.appendChild(btn);
+    }
   }
 }
