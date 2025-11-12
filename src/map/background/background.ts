1a7f5dbe53fff2971601fdc17462b0bfdf8b1ff4
Schiggy <hillbrecht.s@gmail.com>
2025-11-12 22:43:39 +0100

    added anchor

--- src/map/background/background.ts
+++ src/map/background/background.ts
@@ -2,11 +2,13 @@ import type { KAPLAYCtx } from "kaplay";
 
 export default class Background {
   constructor(k: KAPLAYCtx) {
+    // const game = document.querySelector(".game");
+    // const background = document.createElement("img");
+    // background.classList = "background";
+    // background.src = "./background.png";
+    // game?.appendChild(background);
+
     k.loadSprite("background", "/background.png");
-    k.add([
-      k.sprite("background"),
-      k.pos(k.width() / 2, k.height() / 2),
-      k.anchor("center"),
-    ]);
+    k.add([k.sprite("background"), k.anchor("center")]);
   }
 }
