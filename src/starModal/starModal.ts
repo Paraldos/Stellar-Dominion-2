a1fcac8bfe01caf97494f4725525c7f3ce5963f3
Schiggy <hillbrecht.s@gmail.com>
2025-11-16 08:34:12 +0100

    added districts

--- src/starModal/starModal.ts
+++ src/starModal/starModal.ts
@@ -2,6 +2,7 @@ import Modal from "../modal/modal";
 import "./starModal.css";
 import StarData from "../data/starData";
 import ConstructionArea from "./ConstructionArea";
+import { Building } from "../data/building";
 
 export default class StarModal extends Modal {
   starData: StarData;
@@ -11,6 +12,7 @@ export default class StarModal extends Modal {
     this.starData = starData;
     this.addSoladSystemImage();
     this.addStarInformation();
+    this.addDistricts();
     new ConstructionArea(this.modalContent, starData);
   }
 
@@ -28,5 +30,28 @@ export default class StarModal extends Modal {
 		<p>Size: ${this.starData.size}</p>
 	`;
     this.modalContent.appendChild(div);
+  }
+
+  addDistricts(): void {
+    const div = document.createElement("div");
+    div.classList = "starModal__districts";
+    this.modalContent.appendChild(div);
+
+    this.starData.buildings.forEach((building, index) => {
+      if (building.constructionProgress < 100) return;
+      this.addDistrict(building, index, div);
+    });
+  }
+
+  addDistrict(building: Building, index: number, container: HTMLElement) {
+    // content
+    const btn = document.createElement("button");
+    btn.classList = "starModal__district";
+    btn.innerHTML = `
+	<img src="${building.imageSrc}" alt="${building.title}" />
+	<span>Progress: ${building.constructionProgress}</span>
+    <img class="starModal__xImage" src="/public/x.svg" alt="${building.title}" />`;
+    container.appendChild(btn);
+    // logic
   }
 }
