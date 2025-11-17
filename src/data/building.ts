c52dcdea99e4a90a2a0b5499a235d445905e641c
Schiggy <hillbrecht.s@gmail.com>
2025-11-17 19:14:48 +0100

    added once per system point to buildings

--- src/data/building.ts
+++ src/data/building.ts
@@ -2,14 +2,16 @@ export class Building {
   title: string;
   imageSrc: string;
   constructionProgress = 0;
+  oncePerSystem: boolean;
 
-  constructor(title: string, imageSrc: string) {
+  constructor(title: string, imageSrc: string, oncePerSystem: boolean) {
     this.title = title;
     this.imageSrc = imageSrc;
+    this.oncePerSystem = oncePerSystem;
   }
 }
 
 export const listOfBuildings = [
-  new Building("Capitol", "./public/capitol.svg"),
-  new Building("Farm", "./public/farm.svg"),
+  new Building("Capitol", "./public/capitol.svg", true),
+  new Building("Farm", "./public/farm.svg", false),
 ];
